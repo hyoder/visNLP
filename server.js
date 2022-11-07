d4719fdbf6b44e093d98ad4e01c2b91bf68255f7
hyoder <hryoder@wpi.edu>
2022-11-07 17:49:17 -0500

    oh man that's embarrassing lol

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.use( favicon( path.join( __dirname, 'public', 'favicon.ico' ) ) );
 app.use( express.static( path.join( __dirname + '/public' ) ) );
 
 app.get('/',     ( req, res ) => { res.sendFile( '/index.html' ); } );
-app.get('/w2v1', ( req, res ) => { res.sendFile( path.join( __dirname, 'public', 'assets', 'w2v1.html' ) ); } );
-app.get('/w2v2', ( req, res ) => { res.sendFile( '/assets/w2v2.html' ); } );
-app.get('/p2v1', ( req, res ) => { res.sendFile( '/assets/p2v1.html' ); } );
-app.get('/p2v2', ( req, res ) => { res.sendFile( '/assets/p2v2.html' ); } );
-app.get('/adam', ( req, res ) => { res.sendFile( '/assets/adam.html' ); } );
+app.get('/w2v1', ( req, res ) => { res.sendFile( path.join( __dirname, 'public', 'views', 'w2v1.html' ) ); } );
+app.get('/w2v2', ( req, res ) => { res.sendFile( '/views/w2v2.html' ); } );
+app.get('/p2v1', ( req, res ) => { res.sendFile( '/views/p2v1.html' ); } );
+app.get('/p2v2', ( req, res ) => { res.sendFile( '/views/p2v2.html' ); } );
+app.get('/adam', ( req, res ) => { res.sendFile( '/views/adam.html' ); } );
 
 app.use('/', router);
 app.listen(process.env.PORT || 3000, function() { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); } );
