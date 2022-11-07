48327bfa3b3761be12151eaa55bcd426ebd1f995
hyoder <hryoder@wpi.edu>
2022-11-07 17:21:53 -0500

    maybe idk tbh

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use( bodyp.json() );
 app.use( favicon( path.join( __dirname, 'public', 'favicon.ico' ) ) );
 app.use( express.static( path.join( __dirname + '/public' ) ) );
 
-app.get('/',     ( req, res ) => { res.sendFile( path.join( __dirname, 'public',       '/index.html' ) ); } );
-app.get('/w2v1', ( req, res ) => { res.sendFile( path.join( __dirname, 'public/assets', '/w2v1.html' ) ); } );
-app.get('/w2v2', ( req, res ) => { res.sendFile( path.join( __dirname, 'public/assets', '/w2v2.html' ) ); } );
-app.get('/p2v1', ( req, res ) => { res.sendFile( path.join( __dirname, 'public/assets', '/p2v1.html' ) ); } );
-app.get('/p2v2', ( req, res ) => { res.sendFile( path.join( __dirname, 'public/assets', '/p2v2.html' ) ); } );
-app.get('/adam', ( req, res ) => { res.sendFile( path.join( __dirname, 'public/assets', '/adam.html' )); } );
+app.get('/',     ( req, res ) => { res.sendFile( '/index.html' ); } );
+app.get('/w2v1', ( req, res ) => { res.sendFile( path.join( __dirname, 'assets', '/w2v1.html' ) ); } );
+app.get('/w2v2', ( req, res ) => { res.sendFile( path.join( __dirname, 'assets', '/w2v2.html' ) ); } );
+app.get('/p2v1', ( req, res ) => { res.sendFile( path.join( __dirname, 'assets', '/p2v1.html' ) ); } );
+app.get('/p2v2', ( req, res ) => { res.sendFile( path.join( __dirname, 'assets', '/p2v2.html' ) ); } );
+app.get('/adam', ( req, res ) => { res.sendFile( path.join( __dirname, 'assets', '/adam.html' )); } );
 
 app.use('/', router);
 app.listen(process.env.PORT || 3000, function() { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); } );
