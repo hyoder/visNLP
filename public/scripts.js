e4ea1184b2fc99931d6e202a2803de0e2235dbc2
hyoder <hryoder@wpi.edu>
2022-11-07 16:53:24 -0500

    this probably won't work

---
+++ public/scripts.js
@@ -0,0 +1,12 @@
+const homebtn = document.getElementById("navhome"),
+      w2vbtn1 = document.getElementById("navword1"),
+      w2vbtn2 = document.getElementById("navword2"),
+      p2vbtn1 = document.getElementById("navpara1"),
+      p2vbtn2 = document.getElementById("navpara2"),
+      adambtn = document.getElementById("navadam");
+homebtn.addEventListener('click', () => { fetch( '/home', { method:'GET' } ); }, false );
+w2vbtn1.addEventListener('click', () => { fetch( '/w2v1', { method:'GET' } ); }, false );
+w2vbtn2.addEventListener('click', () => { fetch( '/w2v2', { method:'GET' } ); }, false );
+p2vbtn1.addEventListener('click', () => { fetch( '/p2v1', { method:'GET' } ); }, false );
+p2vbtn2.addEventListener('click', () => { fetch( '/p2v2', { method:'GET' } ); }, false );
+adambtn.addEventListener('click', () => { fetch( '/adam', { method:'GET' } ); }, false );
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.use( bodyp.json() );
 app.use( favicon( path.join( __dirname, 'public', 'favicon.ico' ) ) );
 app.use( express.static( path.join( __dirname + '/public' ) ) );
 
-app.get('/', ( req, res ) => { res.render( "index", { msg: "", layout: false } ) } );
+app.get('/',     ( req, res ) => { res.render( "index", { msg: "", layout: false } ); } );
+app.get('/home', ( req, res ) => { res.render( "index", { msg: "", layout: false } ); } );
+app.get('/w2v1', ( req, res ) => { res.render( "w2v1",  { msg: "", layout: false } ); } );
+app.get('/w2v2', ( req, res ) => { res.render( "w2v2",  { msg: "", layout: false } ); } );
+app.get('/p2v1', ( req, res ) => { res.render( "p2v1",  { msg: "", layout: false } ); } );
+app.get('/p2v2', ( req, res ) => { res.render( "p2v2",  { msg: "", layout: false } ); } );
+app.get('/adam', ( req, res ) => { res.render( "adam",  { msg: "", layout: false } ); } );
 
 app.listen(process.env.PORT || 3000, function() { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); } );
