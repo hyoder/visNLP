23cb594e8572cdfa97db8979307dea4c9c156591
hyoder <hryoder@wpi.edu>
2022-11-07 18:15:03 -0500

    maybe??? tf

--- public/scripts.js
+++ public/scripts.js
@@ -4,11 +4,10 @@ const homebtn = document.getElementById( "navhome"  ),
       p2vbtn1 = document.getElementById( "navpara1" ),
       p2vbtn2 = document.getElementById( "navpara2" ),
       adambtn = document.getElementById( "navadam"  );
-/*
+
 homebtn.addEventListener('click', () => { fetch( '/',     { method:'GET' } ).then( response => response.json ).then(); }, false );
 w2vbtn1.addEventListener('click', () => { fetch( '/w2v1', { method:'GET' } ).then( response => response.json ).then(); }, false );
 w2vbtn2.addEventListener('click', () => { fetch( '/w2v2', { method:'GET' } ).then( response => response.json ).then(); }, false );
 p2vbtn1.addEventListener('click', () => { fetch( '/p2v1', { method:'GET' } ).then( response => response.json ).then(); }, false );
 p2vbtn2.addEventListener('click', () => { fetch( '/p2v2', { method:'GET' } ).then( response => response.json ).then(); }, false );
 adambtn.addEventListener('click', () => { fetch( '/adam', { method:'GET' } ).then( response => response.json ).then(); }, false );
-*/
