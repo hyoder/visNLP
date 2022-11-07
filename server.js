96d8630e32784ef15887bf9ebbc99dd23057fb56
hyoder <hryoder@wpi.edu>
2022-11-07 17:53:31 -0500

    trying this

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const express = require( 'express' ),
        router = express.Router();
 app.use( bodyp.json() );
 app.use( favicon( path.join( __dirname, 'public', 'favicon.ico' ) ) );
-app.use( express.static( path.join( __dirname + '/public' ) ) );
+app.use( express.static( path.join( __dirname, 'public', 'views' ) ) );
 
-app.get('/',     ( req, res ) => { res.sendFile( '/index.html' ); } );
-app.get('/w2v1', ( req, res ) => { res.sendFile( path.join( __dirname, 'public', 'views', 'w2v1.html' ) ); } );
-app.get('/w2v2', ( req, res ) => { res.sendFile( '/views/w2v2.html' ); } );
-app.get('/p2v1', ( req, res ) => { res.sendFile( '/views/p2v1.html' ); } );
-app.get('/p2v2', ( req, res ) => { res.sendFile( '/views/p2v2.html' ); } );
-app.get('/adam', ( req, res ) => { res.sendFile( '/views/adam.html' ); } );
+app.get('/',     ( req, res ) => { res.render( "index", { msg: "", layout: false } ); } );
+app.get('/w2v1', ( req, res ) => { res.render(  "w2v1", { msg: "", layout: false } ); } );
+app.get('/w2v2', ( req, res ) => { res.render(  "w2v2", { msg: "", layout: false } ); } );
+app.get('/p2v1', ( req, res ) => { res.render(  "p2v1", { msg: "", layout: false } ); } );
+app.get('/p2v2', ( req, res ) => { res.render(  "p2v2", { msg: "", layout: false } ); } );
+app.get('/adam', ( req, res ) => { res.render(  "adam", { msg: "", layout: false } ); } );
 
 app.use('/', router);
 app.listen(process.env.PORT || 3000, function() { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); } );
