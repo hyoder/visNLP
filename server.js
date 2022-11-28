d8673d68fbd7474440e02d2197c80af479b82b5a
hyoder <hryoder@wpi.edu>
2022-11-28 16:59:57 -0500

    pls!

--- server.js
+++ server.js
@@ -17,12 +17,14 @@ app.get('/p2v1', ( req, res ) => { res.render( 'p2v1', function (err, html) { re
 app.get('/p2v2', ( req, res ) => { res.render( 'p2v2', function (err, html) { res.send(html); } ); } );
 app.get('/adam', ( req, res ) => { res.render( 'adam', function (err, html) { res.send(html); } ); } );
 
+/*
 app.use('/', router);
 app.use('/w2v1', router);
 app.use('/w2v2', router);
 app.use('/p2v1', router);
 app.use('/p2v2', router);
 app.use('/adam', router);
+*/
 
 app.listen( process.env.PORT || 3000, function() { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); } );
 //app.set( "views", path.join( __dirname + "/public/views" ) );
