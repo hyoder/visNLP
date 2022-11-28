e54a343fb5a3083a6d819671de4839382fba3c8f
hyoder <hryoder@wpi.edu>
2022-11-28 17:42:30 -0500

    will this?

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const express = require( 'express' ),
        router = express.Router();
 app.use( bodyp.json() );
 app.use( favicon( path.join( __dirname, 'public', 'favicon.ico' ) ) );
-app.use( express.static( path.join( __dirname + '/public' ) ) );
-app.use( express.static( path.join( __dirname + '/public/views' ) ) );
 
 app.get('/',     ( req, res ) => { res.sendFile( path.join( __dirname + '/public/index.html'      ) ); } );
 app.get('/w2v1', ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/w2v1.html' ) ); } );
@@ -16,6 +14,8 @@ app.get('/w2v2', ( req, res ) => { res.sendFile( path.join( __dirname + '/public
 app.get('/p2v1', ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/p2v1.html' ) ); } );
 app.get('/p2v2', ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/p2v2.html' ) ); } );
 app.get('/adam', ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/adam.html' ) ); } );
+
+app.listen( process.env.PORT || 3000, function() { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); } );
 
 /*
 app.get('/',     ( req, res ) => { res.render(      'index.html', function (err, html) { res.send(html); } ); } );
@@ -34,6 +34,3 @@ app.use('/p2v1', router);
 app.use('/p2v2', router);
 app.use('/adam', router);
 */
-
-app.listen( process.env.PORT || 3000, function() { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); } );
-//app.set( "views", path.join( __dirname + "/public/views" ) );
