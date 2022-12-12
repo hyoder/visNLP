5014ce07caea34f69b10c32bd5cd9b2038437144
hyoder <hryoder@wpi.edu>
2022-12-12 17:17:14 -0500

    maybe?

--- public/scripts/w2v1_script.js
+++ public/scripts/w2v1_script.js
@@ -1,8 +1,7 @@
 const canv = document.getElementById( "w2v_canv" );
 function init_canv()
 {
-    canv.innerHTML  = "";
-    canv.innerHTML += "<h1>begin</h1>";
+    canv.innerHTML  = "<h1>begin</h1>";
     canv.innerHTML += "<div style=\"height:3vh;\"></div>";
     canv.innerHTML += "<div id = \"btn_holder\">";
     canv.innerHTML += "<button id = \"cbow_btn\">CBOW</button>"
@@ -17,9 +16,10 @@ function onehot_canv()
 {
     canv.innerHTML  = "";
 }
-canv.dataset.status.addEventListener( "change", () => {
-    stat = canv.dataset.status;
-         if( stat === "init" )   {   init_canv(); }
-    else if( stat === "onehot" ) { onehot_canv(); }
-} );
-window.onload = function() { canv.dataset.status = "init"; canv.dataset.mode = "init"; }
+function updater( status )
+{
+         if( status === "init" )   {   init_canv(); }
+    else if( status === "onehot" ) { onehot_canv(); }
+}
+canv.dataset.status.addEventListener( "change", () => { updater(canv.dataset.status); } );
+window.onload = function() { updater("init"); }
