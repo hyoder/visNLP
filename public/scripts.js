69b239422802dd3a0d8c5d4f14521ca5738f79e0
hyoder <hryoder@wpi.edu>
2022-12-12 16:31:03 -0500

    does this still work? we shall see

--- public/scripts.js
+++ public/scripts.js
@@ -4,10 +4,11 @@ const homebtn = document.getElementById( "navhome"  ),
       p2vbtn1 = document.getElementById( "navpara1" ),
       p2vbtn2 = document.getElementById( "navpara2" ),
       adambtn = document.getElementById( "navadam"  );
-
+/*
 homebtn.addEventListener('click', () => { fetch( '/',     { method:'GET' } ).then( response => response.json ); }, false );
 w2vbtn1.addEventListener('click', () => { fetch( '/w2v1', { method:'GET' } ).then( response => response.json ); }, false );
 w2vbtn2.addEventListener('click', () => { fetch( '/w2v2', { method:'GET' } ).then( response => response.json ); }, false );
 p2vbtn1.addEventListener('click', () => { fetch( '/p2v1', { method:'GET' } ).then( response => response.json ); }, false );
 p2vbtn2.addEventListener('click', () => { fetch( '/p2v2', { method:'GET' } ).then( response => response.json ); }, false );
 adambtn.addEventListener('click', () => { fetch( '/adam', { method:'GET' } ).then( response => response.json ); }, false );
+*/
