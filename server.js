ef55e399a424c79924c117f1d8dd719ef50da8a6
hyoder <hryoder@wpi.edu>
2022-12-12 16:49:19 -0500

    ok ok ok ok my bad sorry my bad

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ app.use( bodyp.json() );
 app.use( favicon( path.join( __dirname, 'public', 'favicon.ico' ) ) );
 app.use( express.static( path.join( __dirname + '/public' ) ) );
 
-app.get('/',     ( res ) => { res.sendFile( path.join( __dirname + '/public/index.html'      ) ); } );
-app.get('/w2v1', ( res ) => { res.sendFile( path.join( __dirname + '/public/views/w2v1.html' ) ); } );
-app.get('/w2v2', ( res ) => { res.sendFile( path.join( __dirname + '/public/views/w2v2.html' ) ); } );
-app.get('/p2v1', ( res ) => { res.sendFile( path.join( __dirname + '/public/views/p2v1.html' ) ); } );
-app.get('/p2v2', ( res ) => { res.sendFile( path.join( __dirname + '/public/views/p2v2.html' ) ); } );
-app.get('/adam', ( res ) => { res.sendFile( path.join( __dirname + '/public/views/adam.html' ) ); } );
+app.get('/',     ( req, res ) => { res.sendFile( path.join( __dirname + '/public/index.html'      ) ); } );
+app.get('/w2v1', ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/w2v1.html' ) ); } );
+app.get('/w2v2', ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/w2v2.html' ) ); } );
+app.get('/p2v1', ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/p2v1.html' ) ); } );
+app.get('/p2v2', ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/p2v2.html' ) ); } );
+app.get('/adam', ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/adam.html' ) ); } );
 
 app.listen( process.env.PORT || 3000, function()
 {
