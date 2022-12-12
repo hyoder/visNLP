49122b26323cd5e7bf4f797f3db0fb5d6ca3e6c4
hyoder <hryoder@wpi.edu>
2022-12-12 17:45:11 -0500

    ok lets go

--- public/scripts/w2v1_script.js
+++ public/scripts/w2v1_script.js
@@ -7,10 +7,9 @@ function canv_init()
     canv.innerHTML += "<div style=\"height:3vh;\"></div>";
     canv.innerHTML += "<div id = \"btn_holder\">";
     canv.innerHTML += "<button id = \"cbow_btn\">CBOW</button>"
-    canv.innerHTML += "<button id = \"cbow_btn\">CBOW</button>"
     canv.innerHTML += "<button id = \"skip_btn\">skip-gram</button></div>"
     const cbow_btn = document.getElementById( "cbow_btn" ),
-    skip_btn = document.getElementById( "skip_btn" );
+          skip_btn = document.getElementById( "skip_btn" );
     cbow_btn.addEventListener( "click", () => { canv.dataset.status = "onehot"; canv.dataset.mode = "cbow"; } );
     skip_btn.addEventListener( "click", () => { canv.dataset.status = "onehot"; canv.dataset.mode = "skipgram"; } );
 }
@@ -22,9 +21,12 @@ function canv_updater( status )
 {
     switch( status )
     {
-        case "init":    { canv_init(); }
+        case "init":    { canv_init();   }
         case 'onehot':  { canv_onehot(); }
     }
 }
-canv.dataset.status.addEventListener( "change", () => { canv_updater( canv.dataset.status ); } );
+console.log(canv.attributes);
+console.log("ok");
+console.log(canv.dataset);
+//canv.attributes.dataset.status.addEventListener( "change", () => { canv_updater( canv.dataset.status ); } );
 window.onload = (e) => { console.log('init1'); canv_updater("init"); };
