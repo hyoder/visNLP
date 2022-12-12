94d72eb7544b5ac6ae7b3e9185e04170963b823e
hyoder <hryoder@wpi.edu>
2022-12-12 16:41:04 -0500

    ok

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,11 @@
+/*
 const homebtn = document.getElementById( "navhome"  ),
       w2vbtn1 = document.getElementById( "navword1" ),
       w2vbtn2 = document.getElementById( "navword2" ),
       p2vbtn1 = document.getElementById( "navpara1" ),
       p2vbtn2 = document.getElementById( "navpara2" ),
       adambtn = document.getElementById( "navadam"  );
-/*
+
 homebtn.addEventListener('click', () => { fetch( '/',     { method:'GET' } ).then( response => response.json ); }, false );
 w2vbtn1.addEventListener('click', () => { fetch( '/w2v1', { method:'GET' } ).then( response => response.json ); }, false );
 w2vbtn2.addEventListener('click', () => { fetch( '/w2v2', { method:'GET' } ).then( response => response.json ); }, false );
