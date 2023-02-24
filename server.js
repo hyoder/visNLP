b2dbc9401ef0e951a972ee696bbc50caf48a9637
unknown <jprom254@gmail.com>
2023-02-24 18:03:21 -0500

    add initial adam page setups
    add initial adam overview and stepbystep page structures

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ app.use( bodyp.json() );
 app.use( favicon( path.join( __dirname, 'public', 'favicon.ico' ) ) );
 app.use( express.static( path.join( __dirname + '/public' ) ) );
 
-app.get('/',      ( req, res ) => { res.sendFile( path.join( __dirname + '/public/index.html'          ) ); } );
-app.get('/intro', ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/basics.html'    ) ); } );
-app.get('/w2v',   ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/word2vec.html' ) ); } );
-app.get('/s2v',   ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/sen2vec.html'  ) ); } );
-app.get('/p2v',   ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/para2vec.html' ) ); } );
-app.get('/song',  ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/song2vec.html' ) ); } );
-app.get('/news',  ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/news2vec.html' ) ); } );
-app.get('/adam',  ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/adam.html'     ) ); } );
+app.get('/',          ( req, res ) => { res.sendFile( path.join( __dirname + '/public/index.html'          ) ); } );
+app.get('/intro',     ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/basics.html'    ) ); } );
+app.get('/w2v',       ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/word2vec.html' ) ); } );
+app.get('/s2v',       ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/sen2vec.html'  ) ); } );
+app.get('/p2v',       ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/para2vec.html' ) ); } );
+app.get('/song',      ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/song2vec.html' ) ); } );
+app.get('/news',      ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/news2vec.html' ) ); } );
+app.get('/adam',      ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/adam.html'     ) ); } );
+app.get('/adamstep',  ( req, res ) => { res.sendFile( path.join( __dirname + '/public/views/adamstep.html'     ) ); } );
 
 app.listen( process.env.PORT || 3000, function()
 {
